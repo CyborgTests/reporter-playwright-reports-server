4ab05698db8b86bd036df52c05a0ff903807ff66
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2025-06-04 13:27:42 +0300

    linter fix

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,21 @@
 import type { FullConfig, Reporter } from '@playwright/test/reporter';
 export type ReporterOptions = {
-    enabled?: boolean;
-    url: string;
-    reportPath: string;
-    token?: string;
-    resultDetails?: {
-        [key: string]: string;
-    };
-    triggerReportGeneration?: boolean;
+  enabled?: boolean;
+  url: string;
+  reportPath: string;
+  token?: string;
+  resultDetails?: {
+    [key: string]: string;
+  };
+  triggerReportGeneration?: boolean;
 };
 declare class ReporterPlaywrightReportsServer implements Reporter {
-    rpOptions: ReporterOptions;
-    pwConfig: FullConfig;
-    blobPath: string | undefined;
-    blobName: string | undefined;
-    constructor(options: ReporterOptions);
-    onBegin(config: FullConfig): void;
-    onEnd(): Promise<void>;
+  rpOptions: ReporterOptions;
+  pwConfig: FullConfig;
+  blobPath: string | undefined;
+  blobName: string | undefined;
+  constructor(options: ReporterOptions);
+  onBegin(config: FullConfig): void;
+  onEnd(): Promise<void>;
 }
 export default ReporterPlaywrightReportsServer;
--- playwright.config.ts
+++ playwright.config.ts
@@ -6,10 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [
-    ['list'],
-    ['blob', { outputFile: 'test-results/results.zip' }],
-  ],
+  reporter: [['list'], ['blob', { outputFile: 'test-results/results.zip' }]],
   use: {},
   projects: [
     {
